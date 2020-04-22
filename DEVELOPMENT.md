8b187973dec0bae85d7292997c7bf298db483285
jiazhuofan <zhuofan7@illinois.edu>
2020-04-21 23:51:19 -0500

    Included files from external library and compiled it to let it build properly

--- DEVELOPMENT.md
+++ DEVELOPMENT.md
@@ -1,3 +1,8 @@
 # Development
 
 ---
+ - **4/21/20** Tried to set up external libraries
+   - Spent so many hours trying to set up libfacedetection
+   - Still cannot import it as how the writeup in final project does
+   - Include it following the instructions in the lib's readme file
+   - Still need to figure out how to use it to detect specific parts of faces
--- apps/my_app.cc
+++ apps/my_app.cc
@@ -3,10 +3,15 @@
 #include "my_app.h"
 
 #include <cinder/app/App.h>
-#include <facedetectcnn.h>
-#include <facedetectcnn-model.cpp>
-#include <facedetectcnn.cpp>
+#include <rph/NotificationManager.h>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
 
+#include "cinder/gl/Texture.h"
+#include "cinder/Surface.h"
+#include "cinder/ImageIo.h"
+#include "cinder/ip/EdgeDetect.h"
+#include "cinder/ip/Grayscale.h"
 
 namespace myapp {
 
@@ -15,12 +20,15 @@ using cinder::app::KeyEvent;
 MyApp::MyApp() { }
 
 void MyApp::setup() {
-
+  rph::NotificationManager::getInstance()->add("Hello, World!", 10);
 }
 
 void MyApp::update() { }
 
-void MyApp::draw() { }
+void MyApp::draw() {
+  cinder::gl::clear();
+  rph::NotificationManager::getInstance()->draw();
+}
 
 void MyApp::keyDown(KeyEvent event) { }
 
