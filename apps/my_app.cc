d1f781eeb9587517d142d5a44f9734bc83459245
jiazhuofan <zhuofan7@illinois.edu>
2020-04-28 21:07:52 -0500

    Import OpenCV and write a simple video capture app that shows the edge of image from camera

--- apps/my_app.cc
+++ apps/my_app.cc
@@ -17,28 +17,36 @@
 #include "cinder/gl/gl.h"
 #include "cinder/app/RendererGl.h"
 
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+
 namespace myapp {
 
-//using namespace ci;
-
-using cinder::Surface32f;
 using cinder::app::KeyEvent;
-using cinder::vec2;
 
+/*
+using cinder::Surface32f;
+using cinder::vec2;
 cinder::params::InterfaceGl mParams;
 float mContrast, mContrastOld;
 float mBrightness, mBrightnessOld;
 Surface32f mImage, mImageOutput;
+*/
+
+cv::VideoCapture cap(0);
+cv::Mat edges;
 
 MyApp::MyApp() { }
 
 void MyApp::setup() {
+
+  /*
   rph::NotificationManager::getInstance()->add("Hello, World!", 10);
 
   mImage = loadImage(loadAsset("image.png"));
   mImageOutput = cinder::Surface32f(mImage.getWidth(), mImage.getHeight(), false);
 
-//  setWindowSize(1025, 512);
   mContrast = 0.f;
   mContrastOld = -1.f;
   mBrightness = 0.f;
@@ -48,29 +56,48 @@ void MyApp::setup() {
   mParams.addParam("Brightness", &mBrightness, "min=-0.5 max=0.5 step=0.01");
 
 //  cinder::gl::Texture texture = cinder::loadImage( "image.jpg" );
+   */
+
+  cv::namedWindow("edges",1);
+
 }
 
 void MyApp::update() {
+
+  /*
   if (mContrastOld != mContrast || mBrightnessOld != mBrightness) {
     float c = 1.f + mContrast;
 //    Surface32f::IterpixelIter = mImage.getIter();
 //    Surface32f::IterpixelOutIter = mImageOutput.getIter();
   }
+   */
+
+  cv::Mat frame;
+  cap >> frame; // get a new frame from camera
+  cv::cvtColor(frame, edges, cv::COLOR_BGR2GRAY);
+  cv::GaussianBlur(edges, edges, cv::Size(7,7), 1.5, 1.5);
+  cv::Canny(edges, edges, 0, 30, 3);
+
 }
 
 void MyApp::draw() {
   cinder::gl::clear();
+
+  /*
   rph::NotificationManager::getInstance()->draw();
-//cinder::gl::drawSolidCircle(getWindowCenter(), 200);
+
   vec2 center = getWindowCenter();
   float r = 100;
-
   ci::gl::color( ci::Color( 1, 0, 0 ) ); // red
   ci::gl::drawSolidCircle( center + vec2( -r, r ), r );
   ci::gl::color( ci::Color( 0, 1, 0 ) ); // green
   ci::gl::drawSolidCircle( center + vec2( r, r ), r );
   ci::gl::color( ci::Color( 0, 0, 1 ) ); // blue
   ci::gl::drawSolidCircle( center + vec2( 0, -0.73 * r ), r );
+   */
+
+  imshow("edges", edges);
+
 }
 
 void MyApp::keyDown(KeyEvent event) { }
